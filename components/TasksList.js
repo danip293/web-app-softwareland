22870b32c793e5b41284172c38d313df746e7eb0
Daniel Velazquez Giron <danip293@gmail.com>
2021-10-22 14:16:03 -0500

    column actions removed from TaskList

--- components/TasksList.js
+++ components/TasksList.js
@@ -1,26 +1,9 @@
 import React, { useEffect } from 'react';
 import { Table, Container, Row, Col, Button } from 'reactstrap';
 
-const del = (id) =>
-  fetch(`https://api-softwareland-sigma.vercel.app/tasks/${id}`, {
-    method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
-    // mode: 'cors', // no-cors, *cors, same-origin
-    // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    // credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json',
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    // redirect: 'follow', // manual, *follow, error
-    // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    // body: JSON.stringify(data), // body data type must match "Content-Type" header
-  })
-    .then(() => console.log('deleted'))
-    .catch((err) => console.log(err));
+
 
 const TaskList = ({ tasks }) => {
-  console.log(tasks);
-
   return (
     <section className="section" id="tasklist">
       <Container>
@@ -36,7 +19,6 @@ const TaskList = ({ tasks }) => {
                     <th>ID</th>
                     <th>Nombre</th>
                     <th>Status</th>
-                    <th>Acciones</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -45,16 +27,6 @@ const TaskList = ({ tasks }) => {
                       <th scope="row">{task._id}</th>
                       <td>{task.name}</td>
                       <td>{task.status}</td>
-                      <td>
-                        <Button
-                          color="danger"
-                          onClick={(e) => {
-                            del(task._id);
-                          }}
-                        >
-                          Borrar
-                        </Button>
-                      </td>
                     </tr>
                   ))}
                 </tbody>
